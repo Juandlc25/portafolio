d5383a7a18a393fe0a114ed74471b2ef235462e8
Juandlc <juniorelpapa09@hotmail.com>
2020-09-28 13:45:39 -0500

    Updates

---
+++ precache-manifest.1eda316c14ceafb61be12da2f9174be6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5940e67d38d15fa0cd9a325293e43385",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "5dc5d3e437de1b55f48a",
+    "url": "/portafolio/static/css/main.99c32fb2.chunk.css"
+  },
+  {
+    "revision": "b4b8f06f2c4d525673ec",
+    "url": "/portafolio/static/js/2.ae0f7753.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portafolio/static/js/2.ae0f7753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc5d3e437de1b55f48a",
+    "url": "/portafolio/static/js/main.e748c032.chunk.js"
+  },
+  {
+    "revision": "d30650f1ec318e36fc57",
+    "url": "/portafolio/static/js/runtime-main.1d39578f.js"
+  }
+]);
